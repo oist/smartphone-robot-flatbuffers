39c9b2c80f4917dd3b32583c2113cebbc3ba5aef
Christopher Buckley <topherbuckley@gmail.com>
2024-06-25 13:53:23 +0900

    removing unnecessary main dir

--- generate-classes.sh
+++ generate-classes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-flatc -b -j -o src/main/java/ episode.fbs
-flatc -b -p -o src/main/python/ --force-empty-vectors episode.fbs
+flatc -b -j -o src/java/ episode.fbs
+flatc -b -p -o src/python/ --force-empty-vectors episode.fbs
